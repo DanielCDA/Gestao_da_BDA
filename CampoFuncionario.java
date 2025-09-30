8d7cadea2d5487ca45144c1f22fc61aaaab26c7b
Daniel CDA <danielagostinho15@gmail.com>
2025-09-30 14:37:24 +0100

    melhorando o painel adicionar

--- CampoFuncionario.java
+++ CampoFuncionario.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import javax.swing.*;
 
 
@@ -11,8 +12,6 @@ public class CampoFuncionario extends JPanel {
         frame.setSize(800, 600);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
         // Criar o JPanel
         JPanel panel = new JPanel();
 
@@ -20,14 +19,10 @@ public class CampoFuncionario extends JPanel {
         JLabel label = new JLabel("Olá, Daniel!");
         JButton botao = new JButton("Clique aqui");
 
-        
-
         botao.addActionListener(e -> 
         { adicionarFuncionario();   
         });
         
-        
-
 
         // Adicionar o JPanel ao JFrame
         panel.add(label);
@@ -39,25 +34,45 @@ public class CampoFuncionario extends JPanel {
     }
 
     private void adicionarFuncionario() {
-       JFrame frame = new JFrame ("Login");
+       JFrame frame = new JFrame ("Addicionar Funcionario");
         frame.setSize(800,300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
 
+        // Criar os rótulos
         JLabel nome = new JLabel ("Nome:");
-        nome.setBounds(50, 50, 80, 30);
+        nome.setBounds(50, 100, 80, 30);
 
         JLabel senha = new JLabel ("Senha:");
-        senha.setBounds(50, 100, 80, 30);
+        senha.setBounds(50, 150, 80, 30);
 
+        JLabel id = new JLabel ("ID:");
+        id.setBounds(50, 50, 80, 30);
+
+        JLabel telefone = new JLabel ("Telefone:");
+        telefone.setBounds(350, 50, 80, 30);
+        
+        JPanel genero = criarCardGenero();
+        genero.setBounds(350, 100, 280, 50); 
+        
+
+        // Criar os campos de texto
         JTextField  textNome = new JTextField();
         textNome.setBounds(110, 50, 200, 30);
 
         JTextField  textSenha = new JTextField();
         textSenha.setBounds(110, 100, 200, 30);
 
+        JTextField  textId = new JTextField();
+        textId.setBounds(110, 150, 200, 30);
+
+        JTextField numTelefone = new JTextField();
+        numTelefone.setBounds(420, 50, 200, 30);
+
         JButton botao = new JButton ("Entrar");
-        botao.setBounds(250, 200, 80, 30);
+        botao.setBounds(310, 210, 80, 30);
+
+
 
         botao.addActionListener(e -> {
             String nomeDigitado = textNome.getText();
@@ -70,14 +85,48 @@ public class CampoFuncionario extends JPanel {
             }
         });
 
+
         frame.add(nome);
         frame.add(senha);
+        frame.add(id);
+        frame.add(telefone);
+        frame.add(genero);
+
         frame.add(textNome);
         frame.add(textSenha);
+        frame.add(textId);
+        frame.add(numTelefone);
         frame.add(botao);
         frame.setVisible(true);
 
 
+    }
+
+    private JPanel criarCardGenero() 
+    {
+        JPanel card = new JPanel();
+        card.setLayout(new FlowLayout(FlowLayout.LEFT)); 
+        card.setBorder(BorderFactory.createTitledBorder("Gênero"));
+
+        // Opções de gênero
+        JRadioButton masculino = new JRadioButton("Masculino");
+        JRadioButton feminino = new JRadioButton("Feminino");
+        
+        // Grupo (para garantir que só 1 pode ser selecionado)
+        ButtonGroup grupoGenero = new ButtonGroup();
+        grupoGenero.add(masculino);
+        grupoGenero.add(feminino);
+
+        // Adiciona ao painel
+        card.add(masculino);
+        card.add(feminino);
+
+        return card;
+    }
+
+    private Component criarCard(String string, String string2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'criarCard'");
     }
 
     public static void main(String[] args) {
